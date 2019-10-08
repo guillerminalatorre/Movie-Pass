c8db1e654d8a4dd771c9dc4afc1be98cd64cde79
Rodrigo Fanjul <rodrii.fan@gmail.com>
2019-10-08 18:34:26 -0300

    Editado Config.php Agregado index.php, .htaccess

--- Config/Config.php
+++ Config/Config.php
@@ -5,7 +5,7 @@
 
 	define("ROOT", dirname(__DIR__) . "/");
 	//Path to your project's root folder
-	define("FRONT_ROOT", "/TP6/");
+	define("FRONT_ROOT", "/TP-Metodologia/");
 	define("VIEWS_PATH", "Views/");
 	define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "layout/styles/");
 	define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
