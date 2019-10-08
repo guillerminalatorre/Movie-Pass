bbc8e75239bd4a97dd113b25f854e767339edbb7
PamelaBerenz <piberenz@gmail.com>
2019-10-08 18:37:20 -0300

    Adding proyect views

---
+++ Views/Header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="Views/css/estilos.css">
+  <title>Movie Pass App</title>
+</head>
+
+<body>
