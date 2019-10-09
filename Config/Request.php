7db3ced83fae9dea54652b8d570788b7fa4b0e32
Rodrigo Fanjul <rodrii.fan@gmail.com>
2019-10-08 21:27:12 -0300

    Fix Request.php, Router.php

--- Config/Request.php
+++ Config/Request.php
@@ -1,82 +1,73 @@
-<?php
-	/**
-	 * @author Guille
-	 * @version 1.0
-	 * @created 06-oct.-2019 19:05:09
-	 */
-	namespace Config;
-	
-	class Request
-	{
+<?php 
+    namespace Config;
 
-		private $controller;
-		private $method;
-		private $parameters = array();
+    class Request
+    {
+        private $controller;
+        private $method;
+        private $parameters = array();
+        
+        public function __construct()
+        {
+            $url = filter_input(INPUT_GET, "url", FILTER_SANITIZE_URL);
 
-		public function _construct()
-		{
-			$url = filter_input(INPUT_GET, "url", FILTER_SANITIZE_URL);
-		
-						$urlArray = explode("/", $url);
-					
-						$urlArray = array_filter($urlArray);
-		
-						if(empty($urlArray))
-							$this->controller = "Home";            
-						else
-							$this->controller = ucwords(array_shift($urlArray));
-		
-						if(empty($urlArray))
-							$this->method = "Index";
-						else
-							$this->method = array_shift($urlArray);
-		
-						$methodRequest = $this->getMethodRequest();
-									
-						if($methodRequest == "GET")
-						{
-							unset($_GET["url"]);
-		
-							if(!empty($_GET))
-							{                    
-								foreach($_GET as $key => $value)                    
-									array_push($this->parameters, $value);
-							}
-							else
-								$this->parameters = $urlArray;
-						}
-						elseif ($_POST)
-							$this->parameters = $_POST;
-						
-						if($_FILES)
-						{
-							unset($this->parameters["button"]);
-							
-							foreach($_FILES as $file)
-							{
-								array_push($this->parameters, $file);
-							}
-						}
-		}
+            $urlArray = explode("/", $url);
+         
+            $urlArray = array_filter($urlArray);
 
-		public static function getMethodRequest()
-		{
-			return $_SERVER["REQUEST_METHOD"];
-		}
+            if(empty($urlArray))
+                $this->controller = "Home";            
+            else
+                $this->controller = ucwords(array_shift($urlArray));
 
-		public function getController()
-		{
-			return $this->controller;
-		}
+            if(empty($urlArray))
+                $this->method = "Index";
+            else
+                $this->method = array_shift($urlArray);
 
-		public function getMethod()
-		{
-			return $this->method;
-		}
+            $methodRequest = $this->getMethodRequest();
+                        
+            if($methodRequest == "GET")
+            {
+                unset($_GET["url"]);
 
-		public function getParameters()
-		{
-			return $this->parameters;
-		}
-	}
+                if(!empty($_GET))
+                {                    
+                    foreach($_GET as $key => $value)                    
+                        array_push($this->parameters, $value);
+                }
+                else
+                    $this->parameters = $urlArray;
+            }
+            elseif ($_POST)
+                $this->parameters = $_POST;
+            
+            if($_FILES)
+            {
+                unset($this->parameters["button"]);
+                
+                foreach($_FILES as $file)
+                {
+                    array_push($this->parameters, $file);
+                }
+            }
+        }
+
+        private static function getMethodRequest()
+        {
+            return $_SERVER["REQUEST_METHOD"];
+        }            
+
+        public function getController() {
+            return $this->controller;
+        }
+
+        public function getMethod() {
+            return $this->method;
+        }
+
+        public function getparameters() {
+            return $this->parameters;
+        }
+    }
 ?>
