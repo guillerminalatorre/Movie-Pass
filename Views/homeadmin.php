4d4f93ec0f43dab98aca914b1d2c1a72750e0a8f
guillerminalatorre <44948061+guillerminalatorre@users.noreply.github.com>
2019-10-08 21:51:52 -0300

    8/10 homeadmin.php

--- Views/homeadmin.php
+++ Views/homeadmin.php
@@ -1,9 +1,32 @@
+<?php 
+require "Config/Autoload.php"; 
+
+use Config\Autoload as Autoload;
+
+Autoload::start();
+
+use Models\Cine as Cine;
+use DAO\CineDAO as DAO;
+
+require_once("Views/header.php");
+
+?>
+
 <div class="container container-fluid mt-4">
     <h2>Selecciona un cine para operar: </h2>
-    <br>
-    <button type="button" class="btn btn-info btn-block">CINE - Paseo Diagonal - San Martin 1114</button>
-    <button type="button" class="btn btn-info btn-block">CINE - Paseo Diagonal - San Martin 1114</button>
-    <button type="button" class="btn btn-info btn-block">CINE - Paseo Diagonal - San Martin 1114</button>
-    <hr>
-    <button type="button" class="btn btn-success btn-block"><i class="fas fa-plus-circle"></i> Agregar cine</button>
+    <?php 
+        $cineDAO = new CineDAO();
+        $cines = $cineDAO->getAll();
+
+        foreach ($cines as $cine) {
+            ?>
+                <br>
+                <button type="button" class="btn btn-info btn-block" href="ficha-cine.php" value = "<?php $cine->getId();?>">  <?php $cine->getNombre();?></button>
+                <hr>
+            <?php
+        }
+    ?>
+
+    <button type="button" class="btn btn-success btn-block" ><i class="fas fa-plus-circle"></i> Agregar cine</button>
 </div>
+<?php require_once("Views/footer.php");?>
