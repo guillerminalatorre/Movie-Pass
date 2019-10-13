34bfd4d635fe2eaa730f888a3e01dbd1c048bcb1
Rodrigo Fanjul <rodrii.fan@gmail.com>
2019-10-13 19:31:35 -0300

    Mejoras register.php

--- Views/register.php
+++ Views/register.php
@@ -1,56 +1,50 @@
     <div class="container">
         <div class="loginForm">
             <form action="<?php echo FRONT_ROOT ?>Usuario/Register" method="POST">
-                <a class="btn btn-secondary" href="<?php echo FRONT_ROOT ?>Usuario/ShowLoginView" role="button">Volver a login</a>
-                <h2 class="text-left">Ingresa tus datos: </h2>
-                <br>
                 <div class="row">
-                    <div class="form-group col-sm text-right">
-                        <label for="dni">Dni:</label>
+                    <div class="form-group col-sm">
+                        <a class="btn btn-secondary" href="<?php echo FRONT_ROOT ?>Usuario/ShowLoginView" role="button">Volver a login</a>
                     </div>
-                    <div>
+                    <div class="form-group col-sm text-center">
+                        <h2>Registro</h2>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="form-group col-sm">
+                        <label for="dni">Dni:</label>
                         <input type="text" class="form-control" name="dni" required>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="form-group col-sm text-right">
+                    <div class="form-group col-sm">
                         <label for="nombre">Nombre:</label>
-                    </div>
-                    <div>
                         <input type="text" class="form-control" name="nombre" required>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="form-group col-sm text-right">
+                    <div class="form-group col-sm">
                         <label for="apellido">Apellido:</label>
-                    </div>
-                    <div>
                         <input type="text" class="form-control" name="apellido" required>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="form-group col-sm text-right">
+                    <div class="form-group col-sm">
                         <label for="email">Email:</label>
-                    </div>
-                    <div>
                         <input type="email" class="form-control" name="email" placeholder="example@example.com" required>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="form-group col-sm text-right">
-                        <label for="contrase�a">Contrase�a:</label>
-                    </div>
-                    <div>
+                    <div class="form-group col-sm">
+                        <label for="password">Contraseña:</label>
                         <input type="password" class="form-control" name="password" required>
                     </div>
-                    <div class="form-group col-sm text-right">
-                        <label for="contase�a_dos">Repite tu contrase�a:</label>
-                    </div>
-                    <div>
+                    <div class="form-group col-sm">
+                        <label for="confirmpassword">Repite tu contraseña:</label>
                         <input type="password" class="form-control" name="confirmpassword" required>
                     </div>
                     <button type="submit" class="btn btn-primary btn-lg btn-block">Crear cuenta</button>
             </form>
-            <br>
             <button class="btn btn-primary btn-lg btn-block"><i class="fab fa-facebook-f"></i>  Iniciar sesion con Facebook</button>
         </div>
+    </div>
