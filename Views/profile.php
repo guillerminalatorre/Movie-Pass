0dcb8c35dd252c56aa7453016cb7bfd1988656d9
Rodrigo Fanjul <rodrii.fan@gmail.com>
2019-10-17 20:59:16 -0300

    Agregada vista profile.php

--- Views/profile.php
+++ Views/profile.php
@@ -13,42 +13,82 @@ require_once(VIEWS_PATH."navbar.php");
         return preg;
     }
 </script> -->
+<div class="container mb-4">
+    <div class="row">
+        <div class="col-sm bg-light rounded p-4 text-center mr-4">     
+            <a href="#aboutModal" data-toggle="modal" data-target="#myModal"><img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRbezqZpEuwGSvitKy3wrwnth5kysKdRqBW54cAszm_wiutku3R" name="aboutme" width="140" height="140" class="img-circle"></a>
+            <h3 class="mt-2"><?php echo $usuario->getNombre().", ".$usuario->getApellido(); ?></h3>
+            <a href="<?php echo FRONT_ROOT ?>Usuario/ShowModificarUsuario/<?php echo $usuario->getEmail();?>" class="btn btn-warning btn-md mt-2" role="button">Modificar perfil</a>
 
-<div class="container">
-    <div>
-        <!-- <a class="btn btn-secondary" href="<?php echo FRONT_ROOT ?>Usuario/ShowListView" role="button">Volver a lista de usuarios</a>
-        <br> 
-        <br>   -->
-        <table class="table table-sm table-dark">
-            <thead >       
-                <tr>
-                <!-- <th scope="col" style="text-align:left">
-                    <a class="btn btn-warning" onclick = "if(borrarUsuario('<?php echo $usuario->getNombre(); ?>')) href='<?php echo FRONT_ROOT ?>Usuario/eliminarUsuarioYredirect/<?php echo $usuario->getNombre(); ?>' ;">Eliminar Usuario</a>
-                </th> -->
-                <th scope="col" style="text-align:center" >
-                    <h1 class="display-2"><?php echo $usuario->getApellido().", ".$usuario->getNombre(); ?></h1>
-                </th>
-                <th scope="col" style="text-align:right">
-                    <a class="btn btn-info" href="<?php echo FRONT_ROOT ?>Usuario/ShowModificarUsuario/<?php echo $usuario->getEmail();?>">Modificar perfil</a>
-                </th>
-                </tr>
-            </thead>
-        </table>
-        <table class="table table-striped table-dark">
-        <tbody style="text-align:center" >
-            <tr>
-                <th scope="row">Email</th>
-                <td><?php echo $usuario->getEmail(); ?></td>
-            </tr>
-            <tr>
-                <th scope="row">Nombre, Apellido</th>
-                <td><?php echo $usuario->getNombre().", ".$usuario->getApellido(); ?></td>
-            </tr>
-            <tr>
-                <th scope="row">DNI</th>
-                <td><?php echo $usuario->getDni(); ?></td>
-            </tr>
-        </tbody>
-        
+            <ul class="list-group mt-4">
+                <li class="list-group-item">Email: <?php echo $usuario->getEmail(); ?></li>
+                <li class="list-group-item">Nombre: <?php echo $usuario->getNombre().", ".$usuario->getApellido(); ?></li>
+                <li class="list-group-item">DNI: <?php echo $usuario->getDni(); ?></li>
+            </ul>
+        </div>
+        <div class="col-md-8 bg-light rounded p-4">
+            <tr><h4 class="border-bottom border-gray pb-2 mb-0">Lista de entradas</h4></tr>
+            <table class="table table-striped">
+                <thead>
+                    
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
