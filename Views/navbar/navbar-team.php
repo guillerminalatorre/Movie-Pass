3738686e340e8833dd998463e444f326a9c1e2e9
Muaja <rodrii.fan@gmail.com>
2019-11-10 14:50:59 -0300

    Added navbar team.php Added counter to system.php

---
+++ Views/navbar/navbar-team.php
@@ -0,0 +1,3 @@
+<li class="nav-item mx-xl-2">
+    <a class="nav-link text-light" href="<?php echo FRONT_ROOT ?>Team">Equipo</a>
+</li>
--- Views/system/team.php
+++ Views/system/team.php
@@ -6,8 +6,8 @@
             <div class="container py-5">
                 <div class="row text-center text-white">
                     <div class="col-lg-8 mx-auto">
-                        <h1 class="display-4">Equipo de MoviePass</h1>
-                        <p class="lead mb-0">A continuación esta el panel de control de administrador con todos los comandos exclusivos para obtener los datos de la API y de la base de datos.</p>
+                        <h1 class="display-4">Desarrollo de MoviePass</h1>
+                        <h2 class="lead mb-0">Realizado con ♥ por</h2>
                     </div>
                 </div>
             </div><!-- End -->
@@ -17,53 +17,53 @@
                 <div class="row text-center">
 
                     <!-- Team item -->
-                    <div class="col-xl-3 col-6 mb-5">
-                        <div class="bg-light rounded shadow-sm py-5 px-4"><img src="https://d19m59y37dris4.cloudfront.net/university/1-1-1/img/teacher-4.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-                            <h5 class="mb-0">Manuella Nevoresky</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                    <div class="col-xl-3 col-lg-6 col-sm-12 mb-5">
+                        <div class="bg-light rounded shadow-sm py-5 px-4"><img src="<?php echo FRONT_ROOT.IMG_PATH."1.jpg"; ?>" alt="" width="120" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                            <h5 class="mb-0">Pamela Berenz</h5><span class="small text-uppercase text-muted">Pame</span>
                             <ul class="social mb-0 list-inline mt-3">
                                 <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
-                                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
-                                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
-                                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
+                                <li class="list-inline-item"><a href="#" class="social-link text-danger"><i class="fa fa-circle"></i></a></li>
+                                <li class="list-inline-item"><a href="#" class="social-link text-warning"><i class="fa fa-circle"></i></a></li>
+                                <li class="list-inline-item"><a href="#" class="social-link text-success"><i class="fa fa-circle"></i></a></li>
                             </ul>
                         </div>
                     </div><!-- End -->
 
                     <!-- Team item -->
-                    <div class="col-xl-3 col-6 mb-5">
-                        <div class="bg-light rounded shadow-sm py-5 px-4"><img src="https://d19m59y37dris4.cloudfront.net/university/1-1-1/img/teacher-2.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-                            <h5 class="mb-0">Samuel Hardy</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                    <div class="col-xl-3 col-lg-6 col-sm-12 mb-5">
+                        <div class="bg-light rounded shadow-sm py-5 px-4"><img src="<?php echo FRONT_ROOT.IMG_PATH."2.jpg"; ?>" alt="" width="120" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                            <h5 class="mb-0">Rodrigo Fanjul</h5><span class="small text-uppercase text-muted">Carioca</span>
                             <ul class="social mb-0 list-inline mt-3">
                                 <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
-                                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
-                                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
-                                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
+                                <li class="list-inline-item"><a href="#" class="social-link text-danger"><i class="fa fa-circle"></i></a></li>
+                                <li class="list-inline-item"><a href="#" class="social-link text-warning"><i class="fa fa-circle"></i></a></li>
+                                <li class="list-inline-item"><a href="#" class="social-link text-success"><i class="fa fa-circle"></i></a></li>
                             </ul>
                         </div>
                     </div><!-- End -->
 
                     <!-- Team item -->
-                    <div class="col-xl-3 col-6 mb-5">
-                        <div class="bg-light rounded shadow-sm py-5 px-4"><img src="https://d19m59y37dris4.cloudfront.net/university/1-1-1/img/teacher-1.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-                            <h5 class="mb-0">Tom Sunderland</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                    <div class="col-xl-3 col-lg-6 col-sm-12 mb-5">
+                        <div class="bg-light rounded shadow-sm py-5 px-4"><img src="<?php echo FRONT_ROOT.IMG_PATH."3.jpg"; ?>" alt="" width="120" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                            <h5 class="mb-0">Martin Gonzalez</h5><span class="small text-uppercase text-muted">Hawy</span>
                             <ul class="social mb-0 list-inline mt-3">
                                 <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
-                                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
-                                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
-                                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
+                                <li class="list-inline-item"><a href="#" class="social-link text-danger"><i class="fa fa-circle"></i></a></li>
+                                <li class="list-inline-item"><a href="#" class="social-link text-warning"><i class="fa fa-circle"></i></a></li>
+                                <li class="list-inline-item"><a href="#" class="social-link text-success"><i class="fa fa-circle"></i></a></li>
                             </ul>
                         </div>
                     </div><!-- End -->
 
                     <!-- Team item -->
-                    <div class="col-xl-3 col-6 mb-5">
-                        <div class="bg-light rounded shadow-sm py-5 px-4"><img src="https://d19m59y37dris4.cloudfront.net/university/1-1-1/img/teacher-7.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-                            <h5 class="mb-0">John Tarly</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                    <div class="col-xl-3 col-lg-6 col-sm-12 mb-5">
+                        <div class="bg-light rounded shadow-sm py-5 px-4"><img src="<?php echo FRONT_ROOT.IMG_PATH."4.jpg"; ?>" alt="" width="120" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                            <h5 class="mb-0">Guillermina Latorre</h5><span class="small text-uppercase text-muted">Guille</span>
                             <ul class="social mb-0 list-inline mt-3">
                                 <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
-                                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
-                                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
-                                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-circle"></i></a></li>
+                                <li class="list-inline-item"><a href="#" class="social-link text-danger"><i class="fa fa-circle"></i></a></li>
+                                <li class="list-inline-item"><a href="#" class="social-link text-warning"><i class="fa fa-circle"></i></a></li>
+                                <li class="list-inline-item"><a href="#" class="social-link text-success"><i class="fa fa-circle"></i></a></li>
                             </ul>
                         </div>
                     </div><!-- End -->
