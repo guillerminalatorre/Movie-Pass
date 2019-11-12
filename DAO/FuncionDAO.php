de47822341ced2aa99f124526eecdfa90b0ad74b
Muaja <rodrii.fan@gmail.com>
2019-11-12 18:20:22 -0300

    Fix indentation and returns

--- DAO/FuncionDAO.php
+++ DAO/FuncionDAO.php
@@ -191,12 +191,14 @@ use Models\Genero as Genero;
 			}
 		}
 
-		public function getMoviesByGenreAndDate($genre, $date){
-			$list=array();
-			$fecha = date_create($date.' 00:00:00');
-			date_add($fecha, date_interval_create_from_date_string('1 days'));
+		public function getMoviesByGenreAndDate($genre, $date)
+		{
+			try
+			{
+				$list = array();
+				$fecha = date_create($date.' 00:00:00');
+				date_add($fecha, date_interval_create_from_date_string('1 days'));
 
-			try{
 				$query="SELECT DISTINCT f.id_pelicula FROM ".$this->tableName. " f inner join ".$this->peliculasPorGenerosTableName ." pxg on f.id_pelicula=pxg.id_pelicula WHERE pxg.id_genero = ". $genre->getId()." AND fecha_hora BETWEEN '" . $date . "' and '".date_format($fecha, 'Y-m-d H:i:s')."'";
 
 				$this->connection = Connection::GetInstance();
@@ -209,28 +211,33 @@ use Models\Genero as Genero;
 				}
 				return $list;
 			}
-			catch(Exception $ex){
+			catch(Exception $ex)
+			{
 
 			}
 		}
 
-		public function getMoviesWithFunctionsByGenre($genre){
-			$list=array();
-			try{
-				$query="SELECT DISTINCT f.id_pelicula FROM ".$this->tableName. " f inner join ".$this->peliculasPorGenerosTableName ." pxg on f.id_pelicula=pxg.id_pelicula WHERE pxg.id_genero = ". $genre->getId()." and f.fecha_hora >= now()";
+		public function getMoviesWithFunctionsByGenre($genre)
+		{			
+			try
+			{
+				$list = array();
+				$query = "SELECT DISTINCT f.id_pelicula FROM ".$this->tableName. " f inner join ".$this->peliculasPorGenerosTableName ." pxg on f.id_pelicula=pxg.id_pelicula WHERE pxg.id_genero = ". $genre->getId()." and f.fecha_hora >= now()";
 
 				$this->connection = Connection::GetInstance();
 				$resultSet=$this->connection->Execute($query);
 
-				foreach ($resultSet as $row) {
+				foreach ($resultSet as $row) 
+				{
 					$funcion = new Funcion();
 					$funcion->setIdPelicula($row["id_pelicula"]);
 					array_push($list, $funcion);
 				}
 				return $list;
 			}
-			catch(Exception $ex){
-
+			catch(Exception $ex)
+			{
+				return null;
 			}
 		}
 
