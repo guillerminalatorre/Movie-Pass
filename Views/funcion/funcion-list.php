d0b053e7324c809dfdec6aba500ac2b702f8d5a9
PamelaBerenz <piberenz@gmail.com>
2019-11-15 18:36:12 -0300

    Adding max atributte to tickets

--- Views/funcion/funcion-list.php
+++ Views/funcion/funcion-list.php
@@ -20,7 +20,8 @@
     <td><?php echo $funcion->getHora();?></td>
     <td>
     <?php
-    echo count($this->entradaDAO->getByFuncion($funcion));
+    $entradasVendidas = count($this->entradaDAO->getByFuncion($funcion));
+    echo $entradasVendidas;
     echo "/";
     echo $sala->getCapacidad();
     ?>
@@ -29,7 +30,7 @@
         <form action="<?php echo FRONT_ROOT ?>Compra/Pay" method="POST">
             <td style="text-align:right; width:35%">
             <input type="hidden" name="idFuncion" value="<?php echo $funcion->getId(); ?>">
-            <input type="number" class="form-control d-inline align-middle col-2 input-sm" name="cantidad" value="1" min="1" required>
+            <input type="number" class="form-control d-inline align-middle col-2 input-sm" name="cantidad" value="1" min="1" max=<?php echo ($sala->getCapacidad()-$entradasVendidas) ?> required>
 
             <?php if($this->loggedIn() && $esAdmin == false) { ?>            
             <button type="submit" class="btn btn-success shadow-sm align-middle">Comprar entrada</button>
