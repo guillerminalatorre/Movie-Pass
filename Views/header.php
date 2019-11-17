7f13064bef07e8ea961e76b624ff01b6a6ca2dbd
Muaja <rodrii.fan@gmail.com>
2019-11-17 17:52:19 -0300

    Fix integrity issues

--- Views/header.php
+++ Views/header.php
@@ -9,12 +9,12 @@
   <link rel="shortcut icon" href="<?php echo FRONT_ROOT.IMG_PATH ?>favicon.ico" type="image/x-icon">
   <link rel="icon" href="/favicon.ico" type="image/x-icon">
 
-  <link rel="stylesheet" href="<?php echo FRONT_ROOT.CSS_PATH ?>bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo FRONT_ROOT.CSS_PATH ?>bootstrap.min.css">
   <link rel="stylesheet" href="<?php echo FRONT_ROOT.CSS_PATH ?>estilos.css">
   <link rel="stylesheet" href="<?php echo FRONT_ROOT.CSS_PATH ?>all.css">
 
   <script src="<?php echo FRONT_ROOT.JS_PATH ?>jquery.min.js"></script>
-  <script src="<?php echo FRONT_ROOT.JS_PATH ?>bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="<?php echo FRONT_ROOT.JS_PATH ?>bootstrap.min.js"></script>
   <script src="<?php echo FRONT_ROOT.JS_PATH ?>card.js"></script>
   <script src="<?php echo FRONT_ROOT.JS_PATH ?>scripts.js"></script>
 
