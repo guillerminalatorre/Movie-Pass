c62fcd1cec35779b43b41231ba3fa6efff3568b7
Muaja <rodrii.fan@gmail.com>
2019-11-17 23:21:56 -0300

    Searchbar refactor

--- Views/pelicula/searchbar.php
+++ Views/pelicula/searchbar.php
@@ -6,26 +6,25 @@
             <form action="<?php echo FRONT_ROOT ?>Funcion/FilterFunctions" method="POST">
                 <div class="row">
                     <div class="col-xs-12 col-sm-12 col-md-8 col-lg-6">
-                        <h3 class="border-bottom pb-2">Filtrar por categoria/fecha:</h3>
+                        <h3 class="border-bottom pb-2">Filtrar funciones:</h3>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="btn-group col-xs-12 col-sm-6 col-md-6 col-lg-6 mb-2">
+                    <div class="btn-group col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 mb-2">
                         <select name="genreId" class="form-control ">
-                            <option value="none" selected>Elije una categoria...</option>
-                            <?php
-                            foreach ($generoList as $generoValue) {
-                                ?>
-                                <option value="<?php echo $generoValue->getId(); ?>"> <?php echo $generoValue->getNombre(); ?></option>
-                            <?php
-                            }
-                            ?>
+                            <option value="NULL" <?php if(!isset($genreId)) { echo "selected"; } ?>>Categorias</option>
+                            <?php foreach ($generoList as $generoValue) { ?>
+                                <option value="<?php echo $generoValue->getId(); ?>" <?php if(isset($genreId) && $genreId == $generoValue->getId()) { echo "selected"; } ?>> <?php echo $generoValue->getNombre(); ?></option>
+                            <?php } ?>
                         </select>
                     </div>
-                    <div class="btn-group input-group col-xs-12 col-sm-6 col-md-6 col-lg-3 mb-2">
-                        <input type="date" name="chosenDate" min="<?php echo date("Y-m-d")?>" class="form-control">
+                    <div class="btn-group input-group col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 mb-2">
+                        <input type="date" name="fechaInicio" placeholder="Inicio:" min="<?php echo date("Y-m-d")?>" value="<?php if(isset($fechaInicio) && $fechaInicio != null) { echo $fechaInicio; } ?>" class="form-control">
                     </div>
-                    <div class="btn-group col-xs-12 col-sm-6 col-md-6 col-lg-3 mb-2">
+                    <div class="btn-group input-group col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 mb-2">
+                        <input type="date" name="fechaFin" placeholder="Fin:" min="<?php echo date("Y-m-d")?>" value="<?php if(isset($fechaFin) && $fechaFin != null) echo $fechaFin; ?>" class="form-control">
+                    </div>
+                    <div class="btn-group col-xs-12 col-sm-6 col-md-12 col-lg-12 col-xl-3 mb-2">
                         <button type="submit" class="btn btn-success shadow-sm">Buscar</button>
                     </div>
                 </div>
@@ -33,3 +32,14 @@
         </div>
     </div>
 </div>
+<style>
+input[type="date"]:before {
+    content: attr(placeholder) !important;
+    color: #aaa;
+    margin-right: 0.5em;
+  }
+  input[type="date"]:focus:before,
+  input[type="date"]:valid:before {
+    content: "";
+  }
+</style>
