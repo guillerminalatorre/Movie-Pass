a9609bb4b737f3753e0a653f692a9102569354fe
Muaja <rodrii.fan@gmail.com>
2019-11-18 22:15:53 -0300

    Adding datatables dependencies

--- Views/header.php
+++ Views/header.php
@@ -10,12 +10,16 @@
   <link rel="icon" href="/favicon.ico" type="image/x-icon">
 
   <link rel="stylesheet" href="<?php echo FRONT_ROOT.CSS_PATH ?>bootstrap.min.css">
-  <link rel="stylesheet" href="<?php echo FRONT_ROOT.CSS_PATH ?>estilos.css">
   <link rel="stylesheet" href="<?php echo FRONT_ROOT.CSS_PATH ?>all.css">
+  <link rel="stylesheet" href="<?php echo FRONT_ROOT.CSS_PATH ?>dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?php echo FRONT_ROOT.CSS_PATH ?>estilos.css">
+
 
   <script src="<?php echo FRONT_ROOT.JS_PATH ?>jquery.min.js"></script>
   <script src="<?php echo FRONT_ROOT.JS_PATH ?>bootstrap.min.js"></script>
   <script src="<?php echo FRONT_ROOT.JS_PATH ?>card.js"></script>
+  <script src="<?php echo FRONT_ROOT.JS_PATH ?>jquery.dataTables.min.js"></script>
+  <script src="<?php echo FRONT_ROOT.JS_PATH ?>dataTables.bootstrap4.min.js"></script>
   <script src="<?php echo FRONT_ROOT.JS_PATH ?>scripts.js"></script>
 
   <title>Movie Pass App</title>
