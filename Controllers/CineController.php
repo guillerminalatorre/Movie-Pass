a2b38711f5db2075779c511243008381a3bdc163
Muaja <rodrii.fan@gmail.com>
2019-11-20 20:17:46 -0300

    Added manejo excepcion

--- Controllers/CineController.php
+++ Controllers/CineController.php
@@ -35,6 +35,7 @@
 			if(!$this->isAdmin()) Functions::redirect("Home");
 
 			$cineList = $this->cineDAO->getAll();
+			if($cineList == null) Functions::flash("Se produjo un error al obtener los cines.","warning");
 			require_once(VIEWS_PATH."cine/cine-list.php");
 		}
 
